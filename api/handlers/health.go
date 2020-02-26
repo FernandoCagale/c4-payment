93f6651c5f4f729ccfe4988df8ecd0a93492381c
FernandoCagale <fernando.b.cagale@gmail.com>
2020-02-26 20:00:30 -0300

    add health to rabbitmq and mongodb

--- api/handlers/health.go
+++ api/handlers/health.go
@@ -1,17 +1,43 @@
 package handlers
 
 import (
-	"github.com/FernandoCagale/c4-payment/api/render"
+	"github.com/hellofresh/health-go"
+	"gopkg.in/mgo.v2"
+	"os"
+
+	healthRabbitmq "github.com/hellofresh/health-go/checks/rabbitmq"
+
 	"net/http"
+	"time"
 )
 
 type HealthHandler struct {
+	session *mgo.Session
+}
+
+func NewHealth(session *mgo.Session) *HealthHandler {
+	return &HealthHandler{session}
 }
 
 func (handler *HealthHandler) Health(w http.ResponseWriter, r *http.Request) {
-	render.Response(w, map[string]bool{"ok": true}, http.StatusOK)
-}
+	health.Register(health.Config{
+		Name:    "rabbitmq",
+		Timeout: time.Second * 5,
+		Check: healthRabbitmq.New(healthRabbitmq.Config{
+			DSN: os.Getenv("AMQP_URI"),
+		}),
+	})
 
-func NewHealth() *HealthHandler {
-	return &HealthHandler{}
+	health.Register(health.Config{
+		Name: "mongodb",
+		Check: func() error {
+			if err := handler.session.Ping(); err != nil {
+				handler.session.Refresh()
+				return err
+			}
+			return nil
+		},
+	})
+
+	health.HandlerFunc(w, r)
 }
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible
 	github.com/google/wire v0.3.0
 	github.com/gorilla/mux v1.7.3
+	github.com/hellofresh/health-go v2.0.2+incompatible
 	github.com/joho/godotenv v1.3.0
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
 )
